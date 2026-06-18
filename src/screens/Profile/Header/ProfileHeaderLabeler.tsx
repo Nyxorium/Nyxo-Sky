0945d207533c59f416616ff6d5e70389db59e300
RetroSunstar <57507616+RetroSunstar@users.noreply.github.com>
2026-06-18 03:41:12 +0100

    Added notification bell to labeller profiles (#17)

--- src/screens/Profile/Header/ProfileHeaderLabeler.tsx
+++ src/screens/Profile/Header/ProfileHeaderLabeler.tsx
@@ -25,6 +25,7 @@ import {usePreferencesQuery} from '#/state/queries/preferences'
 import {useRequireAuth, useSession} from '#/state/session'
 import {ProfileMenu} from '#/view/com/profile/ProfileMenu'
 import {atoms as a, tokens, useTheme} from '#/alf'
+import {SubscribeProfileButton} from '#/components/activity-notifications/SubscribeProfileButton'
 import {Button, ButtonIcon, ButtonText} from '#/components/Button'
 import {type DialogOuterProps, useDialogControl} from '#/components/Dialog'
 import {MessageProfileButton} from '#/components/dms/MessageProfileButton'
@@ -127,7 +128,10 @@ let ProfileHeaderLabeler = ({
         <View
           style={[a.flex_row, a.justify_end, a.align_center, a.gap_xs, a.pb_lg]}
           pointerEvents={IS_IOS ? 'auto' : 'box-none'}>
-          <HeaderLabelerButtons profile={profile} />
+          <HeaderLabelerButtons
+            profile={profile}
+            moderationOpts={moderationOpts}
+          />
         </View>
         <View style={[a.flex_col, a.gap_2xs, a.pt_2xs, a.pb_md]}>
           <ProfileHeaderDisplayName profile={profile} moderation={moderation} />
@@ -251,9 +255,11 @@ function CantSubscribePrompt({
 
 export function HeaderLabelerButtons({
   profile,
+  moderationOpts,
   minimal = false,
 }: {
   profile: Shadow<AppBskyActorDefs.ProfileViewDetailed>
+  moderationOpts: ModerationOpts
   /** disable the subscribe button */
   minimal?: boolean
 }) {
@@ -306,8 +312,33 @@ export function HeaderLabelerButtons({
         ax.logger.error(`Failed to subscribe to labeler`, {message: e.message})
       }
     })
+
+  const subscriptionsAllowed = useMemo(() => {
+    switch (profile.associated?.activitySubscription?.allowSubscriptions) {
+      case 'followers':
+      case undefined:
+        return !!profile.viewer?.following
+      case 'mutuals':
+        return !!profile.viewer?.following && !!profile.viewer.followedBy
+      case 'none':
+      default:
+        return false
+    }
+  }, [profile])
+
   return (
     <>
+      {hasSession &&
+        !isMe &&
+        !profile.viewer?.blockedBy &&
+        subscriptionsAllowed && (
+          <SubscribeProfileButton
+            profile={profile}
+            moderationOpts={moderationOpts}
+            disableHint={minimal}
+          />
+        )}
+
       {hasSession && !isMe && (!minimal || profile.viewer?.following) && (
         <>
           <MessageProfileButton profile={profile} />
