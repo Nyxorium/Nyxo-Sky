cdb6ceab8fc6fb3b3cabc5936678b395a6585b77
RetroSunstar <57507616+RetroSunstar@users.noreply.github.com>
2026-07-28 03:40:08 +0100

    Added dedicated labellers tab to starter packs (#59)
    * Added dedicated labellers tab to Starter packs
    
    * Updated locales

---
+++ src/components/StarterPack/Main/LabelersList.tsx
@@ -0,0 +1,108 @@
+import {forwardRef, useCallback, useImperativeHandle, useState} from 'react'
+import {type ListRenderItemInfo, View} from 'react-native'
+import {type AppBskyActorDefs, type ModerationOpts} from '@atproto/api'
+
+import {useBottomBarOffset} from '#/lib/hooks/useBottomBarOffset'
+import {useInitialNumToRender} from '#/lib/hooks/useInitialNumToRender'
+import {List, type ListRef} from '#/view/com/util/List'
+import {type SectionRef} from '#/screens/Profile/Sections/types'
+import {atoms as a, useTheme} from '#/alf'
+import {ListFooter, ListMaybePlaceholder} from '#/components/Lists'
+import {Default as ProfileCard} from '#/components/ProfileCard'
+import {IS_NATIVE, IS_WEB} from '#/env'
+
+function keyExtractor(item: AppBskyActorDefs.ProfileView, index: number) {
+  return `${item.did}-${index}`
+}
+
+interface LabelersListProps {
+  profiles?: AppBskyActorDefs.ProfileView[]
+  isError: boolean
+  refetch: () => Promise<unknown>
+  moderationOpts: ModerationOpts
+  headerHeight: number
+  scrollElRef: ListRef
+}
+
+export const LabelersList = forwardRef<SectionRef, LabelersListProps>(
+  function LabelersListImpl(
+    {profiles, isError, refetch, moderationOpts, headerHeight, scrollElRef},
+    ref,
+  ) {
+    const t = useTheme()
+    const bottomBarOffset = useBottomBarOffset(headerHeight)
+    const initialNumToRender = useInitialNumToRender()
+
+    const [isPTRing, setIsPTRing] = useState(false)
+
+    const onScrollToTop = useCallback(() => {
+      scrollElRef.current?.scrollToOffset({
+        animated: IS_NATIVE,
+        offset: -headerHeight,
+      })
+    }, [scrollElRef, headerHeight])
+
+    useImperativeHandle(ref, () => ({
+      scrollToTop: onScrollToTop,
+    }))
+
+    const renderItem = ({
+      item,
+      index,
+    }: ListRenderItemInfo<AppBskyActorDefs.ProfileView>) => {
+      return (
+        <View
+          style={[
+            a.p_lg,
+            t.atoms.border_contrast_low,
+            (IS_WEB || index !== 0) && a.border_t,
+          ]}>
+          <ProfileCard
+            profile={item}
+            moderationOpts={moderationOpts}
+            logContext="StarterPackProfilesList"
+          />
+        </View>
+      )
+    }
+
+    if (!profiles) {
+      return (
+        <View
+          style={[
+            a.h_full_vh,
+            {marginTop: headerHeight, marginBottom: bottomBarOffset},
+          ]}>
+          <ListMaybePlaceholder
+            isLoading={true}
+            isError={isError}
+            onRetry={refetch}
+          />
+        </View>
+      )
+    }
+
+    return (
+      <List
+        data={profiles}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        ref={scrollElRef}
+        headerOffset={headerHeight}
+        ListFooterComponent={
+          <ListFooter
+            style={{paddingBottom: bottomBarOffset, borderTopWidth: 0}}
+          />
+        }
+        showsVerticalScrollIndicator={false}
+        desktopFixedHeight
+        initialNumToRender={initialNumToRender}
+        refreshing={isPTRing}
+        onRefresh={() => {
+          setIsPTRing(true)
+          void refetch().finally(() => setIsPTRing(false))
+        }}
+      />
+    )
+  },
+)
