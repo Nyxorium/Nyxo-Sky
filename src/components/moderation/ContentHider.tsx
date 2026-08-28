affda5f1e854780b590f26e2ca43a19e34220762
RetroSunstar <57507616+RetroSunstar@users.noreply.github.com>
2026-08-28 14:17:04 +0100

    Remove needless redefinitions in ContentHider and Moderation Casue Description

--- src/components/moderation/ContentHider.tsx
+++ src/components/moderation/ContentHider.tsx
@@ -207,12 +207,6 @@ function ContentHiderActive({
         }
 
         const def = cause.labelDef || getDefinition(labelDefs, cause.label)
-        if (def.identifier === 'porn') {
-          return l`Adult Content`
-        }
-        if (def.identifier === 'sexual') {
-          return l`Sexually Suggestive`
-        }
         return getLabelStrings(i18n.locale, globalLabelStrings, def).name
       })
 
